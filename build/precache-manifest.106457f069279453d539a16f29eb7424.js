54632db88d85f53c479c3379bd7991ffef0ac455
nicoculotta <nicoculotta@gmail.com>
2022-03-19 05:42:05 -0300

    result and build

---
+++ build/precache-manifest.106457f069279453d539a16f29eb7424.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d1c79c9978cb94555d824cf560856445",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9956c43200e4a27ef8f4",
+    "url": "/static/css/2.68f0c4ae.chunk.css"
+  },
+  {
+    "revision": "28321290616c6d422bff",
+    "url": "/static/css/main.f2b757bd.chunk.css"
+  },
+  {
+    "revision": "9956c43200e4a27ef8f4",
+    "url": "/static/js/2.b781efda.chunk.js"
+  },
+  {
+    "revision": "4cf5c6c6f464cc15458a86cbdc669a15",
+    "url": "/static/js/2.b781efda.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "28321290616c6d422bff",
+    "url": "/static/js/main.7ef21550.chunk.js"
+  },
+  {
+    "revision": "542cbb7c0c0bafdf377f",
+    "url": "/static/js/runtime-main.45c10d22.js"
+  },
+  {
+    "revision": "b7c9e1e479de3b53f1e4e30ebac2403a",
+    "url": "/static/media/slick.b7c9e1e4.woff"
+  },
+  {
+    "revision": "ced611daf7709cc778da928fec876475",
+    "url": "/static/media/slick.ced611da.eot"
+  },
+  {
+    "revision": "d41f55a78e6f49a5512878df1737e58a",
+    "url": "/static/media/slick.d41f55a7.ttf"
+  },
+  {
+    "revision": "f97e3bbf73254b0112091d0192f17aec",
+    "url": "/static/media/slick.f97e3bbf.svg"
+  }
+]);
